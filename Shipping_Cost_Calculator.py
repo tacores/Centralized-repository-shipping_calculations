5413c0f59c631b9a7d6fc7d265a107f1a14e4681
yuki <yuki@gmai.com>
2024-06-10 11:02:58 +0900

    Revert "another update"
    This reverts commit 20008aee0b542193d9b9d5d0953ff57cd64c4a74.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,7 +1,5 @@
 # Shipping Cost Calculator
- # Here is a new update by tacoresg
-
-# Here is another update by tacores
+ # Here is a new update by tacores
 
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
